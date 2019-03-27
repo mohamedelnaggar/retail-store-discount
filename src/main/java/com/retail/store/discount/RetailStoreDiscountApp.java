e975f5a3fb17404955e5e1f2622e0a8de1e6121d
mohamed elnaggar <mohamed.nn90@gmail.com>
2019-03-28 00:28:55 +0200

    Added spring boot dependencies

---
+++ src/main/java/com/retail/store/discount/RetailStoreDiscountApp.java
@@ -0,0 +1,13 @@
+package com.retail.store.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreDiscountApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailStoreDiscountApp.class, args);
+    }
+
+}
